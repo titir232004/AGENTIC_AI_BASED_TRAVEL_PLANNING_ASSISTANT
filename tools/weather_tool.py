9d68505445da2a77fa215cb1ef1d91d5b8727788
titir232004 <162278370+titir232004@users.noreply.github.com>
2025-12-21 12:14:44 +0530

    Update weather_tool.py

--- tools/weather_tool.py
+++ tools/weather_tool.py
@@ -1,16 +1,27 @@
 import requests
 
+def get_weather(city: str):
+    geo_url = (
+        f"https://geocoding-api.open-meteo.com/v1/search"
+        f"?name={city}&count=1"
+    )
+    geo_res = requests.get(geo_url).json()
 
-def get_weather(lat, lon):
-    url = (
+    if "results" not in geo_res:
+        return {"error": "Location not found"}
+
+    lat = geo_res["results"][0]["latitude"]
+    lon = geo_res["results"][0]["longitude"]
+
+    weather_url = (
         "https://api.open-meteo.com/v1/forecast"
         f"?latitude={lat}&longitude={lon}"
         "&daily=temperature_2m_max"
         "&timezone=auto"
     )
+    weather_res = requests.get(weather_url).json()
 
-    r = requests.get(url).json()
-    temps = r["daily"]["temperature_2m_max"]
+    temps = weather_res["daily"]["temperature_2m_max"]
 
     return {
         "day_1": f"{temps[0]}°C",
